7b3639b5d4b0a1f44c0eb01b7fc38d1526de3b55
Jon Cinque <jon.cinque@gmail.com>
2021-05-16 22:35:54 +0200

    Finish first round of work

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,82 @@
 use {
+    clap::{App, Arg},
+    log::{debug, info},
     reqwest,
-    scraper::{node::Element, Html, Selector},
+    scraper::{Html, Selector},
     serde::Deserialize,
-    serde_json::Value,
 };
 
-const URL: &str = "https://www.doctolib.fr/vaccination-covid-19/75002-paris?ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&force_max_limit=2";
-
 #[derive(Deserialize, Debug)]
-pub struct DetailResponse {
+struct DetailResponse {
     availabilities: Vec<AvailabilityResponse>,
+    search_result: CenterResponse,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct AvailabilityResponse {
+struct AvailabilityResponse {
     date: String,
-    slots: Vec<Value>, // TODO extract real data
+    slots: Vec<SlotResponse>,
 }
 
-fn get_center_id(element: &Element) -> &str {
-    let id_string = element.id().unwrap();
-    id_string.rsplit('-').next().unwrap()
+#[derive(Deserialize, Debug)]
+struct SlotResponse {
+    agenda_id: u32,
+    start_date: String,
+    end_date: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct CenterResponse {
+    address: String,
+    city: String,
+    name_with_title: String,
+    zipcode: String,
+}
+
+fn get_center_id(element_id: &str) -> &str {
+    element_id.rsplit('-').next().unwrap()
+}
+
+fn app() -> App<'static, 'static> {
+    App::new("Doctoscrape")
+        .version("0.1")
+        .author("Jon C. <me@jonc.dev>")
+        .about("Scrapes Doctolib for available appointments, prints out the matches")
+        .arg(
+            Arg::with_name("postal_code")
+                .help("Postal code in which to perform the search")
+                .required(true)
+                .index(1)
+                .value_name("POSTAL_CODE"),
+        )
+        .arg(
+            Arg::with_name("city")
+                .help("City name in which to perform the search")
+                .short("c")
+                .long("city")
+                .default_value("paris")
+                .value_name("CITY"),
+        )
+        .arg(
+            Arg::with_name("exclude_postal_code")
+                .help("Exclude centers at the given postal code")
+                .short("x")
+                .long("exclude")
+                .value_name("POSTAL_CODE"),
+        )
 }
 
 #[tokio::main]
 async fn main() {
-    let resp = reqwest::get(URL).await.unwrap();
+    env_logger::init();
+
+    let matches = app().get_matches();
+    let postal_code = matches.value_of("postal_code").unwrap();
+    let city = matches.value_of("city").unwrap();
+    let exclude_postal_code = matches.value_of("exclude_postal_code");
+    let search_url = format!("https://www.doctolib.fr/vaccination-covid-19/{}-{}?ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&force_max_limit=2", postal_code, city);
+    let resp = reqwest::get(search_url).await.unwrap();
+
     assert!(resp.status().is_success());
 
     let body = resp.text().await.unwrap();
@@ -36,17 +87,40 @@ async fn main() {
 
     // iterate over elements matching our selector
     for result in fragment.select(&results) {
-        let summary = result.text().collect::<Vec<_>>().join(", ");
-
         // get the center's id
-        let id = get_center_id(result.value());
-        println!("{:?}", summary);
-        println!("{:?}", id);
+        let id = get_center_id(result.value().id().unwrap());
 
-        let details_response = reqwest::get(format!( "https://www.doctolib.fr/search_results/{}.json?limit=4&ref_visit_motive_ids%5B%5D=6970&ref_visit_motive_ids%5B%5D=7005&speciality_id=5494&search_result_format=json", id)).await.unwrap();
+        let details_response = reqwest::get(format!( "https://www.doctolib.fr/search_results/{}.json?limit=4&ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&speciality_id=5494&search_result_format=json", id)).await.unwrap();
 
-        let details = details_response.json::<DetailResponse>().await.unwrap();
-        println!("{:?}", details.availabilities);
+        debug!("{:?}", result.text().collect::<Vec<_>>().join(", "));
+        let DetailResponse {
+            search_result,
+            availabilities,
+            ..
+        } = details_response.json().await.unwrap();
+        if let Some(exclude_postal_code) = exclude_postal_code {
+            if search_result.zipcode == exclude_postal_code {
+                continue;
+            }
+        }
+        let mut times = vec![];
+        for availability in availabilities {
+            times.extend(
+                availability
+                    .slots
+                    .into_iter()
+                    .map(|x| x.start_date)
+                    .collect::<Vec<_>>(),
+            );
+        }
+        if !times.is_empty() {
+            let times = times.join("\n");
+            let address = format!("{}, {}", search_result.address, search_result.zipcode);
+            info!(
+                "{} at {} has slots!\n{}",
+                search_result.name_with_title, address, times
+            );
+        }
     }
 }
 
@@ -57,7 +131,34 @@ mod tests {
     #[test]
     fn deserialize_details() {
         let data_string = r#"{
-          "availabilities": [],
+          "availabilities": [
+            { "date": "2021-05-16", "slots": [] },
+            { "date": "2021-05-17", "slots": [
+                {
+                  "agenda_id": 441940,
+                  "end_date": "2021-05-17T09:12:00.000+02:00",
+                  "practitioner_agenda_id": null,
+                  "start_date": "2021-05-17T09:06:00.000+02:00",
+                  "steps": [
+                    {
+                      "agenda_id": 441940,
+                      "end_date": "2021-05-17T09:12:00.000+02:00",
+                      "practitioner_agenda_id": null,
+                      "start_date": "2021-05-17T09:06:00.000+02:00",
+                      "visit_motive_id": 2700183
+                    },
+                    {
+                      "agenda_id": 439320,
+                      "end_date": "2021-06-25T08:36:00.000+02:00",
+                      "practitioner_agenda_id": null,
+                      "start_date": "2021-06-25T08:30:00.000+02:00",
+                      "visit_motive_id": 2700184
+                    }
+                  ]
+                }
+              ]
+            }
+          ],
           "message": "Aucun rendez-vous n'est disponible pour le moment mais de nombreux créneaux vont être mis en ligne dans les jours à venir. Vous pouvez également prendre rendez-vous dans un autre lieu de vaccination.",
           "number_future_vaccinations": 1654,
           "reason": "no_availabilities",
@@ -98,6 +199,29 @@ mod tests {
           "total": 0
         }"#;
         let details: DetailResponse = serde_json::from_str(data_string).unwrap();
-        assert!(details.availabilities.is_empty());
+        assert_eq!(details.search_result.zipcode, "75005");
+        assert_eq!(details.search_result.address, "19b Place du Panthéon");
+        assert_eq!(details.search_result.city, "Paris");
+        assert_eq!(
+            details.search_result.name_with_title,
+            "Centre COVID - Paris 5"
+        );
+        assert_eq!(details.availabilities.len(), 2);
+        let first_availability = &details.availabilities[0];
+        assert_eq!(first_availability.date, "2021-05-16");
+        assert!(first_availability.slots.is_empty());
+        let second_availability = &details.availabilities[1];
+        assert_eq!(second_availability.date, "2021-05-17");
+        assert_eq!(second_availability.slots.len(), 1);
+        assert_eq!(
+            second_availability.slots[0].start_date,
+            "2021-05-17T09:06:00.000+02:00"
+        );
+    }
+
+    #[test]
+    fn extract_id() {
+        let element_id = "search-result-123";
+        assert_eq!(get_center_id(&element_id), "123");
     }
 }
